2e02e1cfa955baae32d628996b5017f0eab8a5d7
Elperro <carlatrvigo@gmail.com>
2024-07-26 01:05:11 +0200

    añadir peña

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const images = [
 	"http://www.aviationcorner.net/public/photos/1/3/avc_00604913.jpg"
 ];
 
-const sonidos = [
-	"audios/150.opus",
-	"audios/aamaricon.opus",
-	"audios/Falacias.opus",
-	"audios/Otto.opus"
+const sonidosPino = [
+	"audiosPino/150.opus",
+	"audiosPino/aamaricon.opus",
+	"audiosPino/Falacias.opus",
+	"audiosPino/Otto.opus"
+];
+
+const sonidosFoca = [
+	"audiosFoca/..."
 ];
 
 // RandomIndex selecciona un número aleatorio (entre 0 y 1) y lo multiplica por la longitud del array de imágenes.
@@ -20,9 +24,18 @@ const randomImage = Math.floor(Math.random() * images.length);
 const body = document.querySelector("body");
 body.style.backgroundImage = `url(${images[randomImage]})`;
 
-const imagen = document.getElementById("pino");
-imagen.addEventListener("click", () => {
-	const randomAudio = Math.floor(Math.random() * sonidos.length);
-	const audio = new Audio(sonidos[randomAudio]);
+// Audios de pino
+const imagenPino = document.getElementById("pino");
+imagenPino.addEventListener("click", () => {
+	const randomAudio = Math.floor(Math.random() * sonidosPino.length);
+	const audio = new Audio(sonidosPino[randomAudio]);
+	audio.play();
+});
+
+// Audios de foca
+const imagenFoca = document.getElementById("foca");
+imagenFoca.addEventListener("click", () => {
+	const randomAudio = Math.floor(Math.random() * sonidosFoca.length);
+	const audio = new Audio(sonidosFoca[randomAudio]);
 	audio.play();
 });
